e7238de297df97091ed50392e8c7acbb350dc8f1
yss0512 <dbtntkd456@naver.com>
2025-06-08 02:30:18 +0900

    [FEAT] 회원가입 구현
    - MemberController: 인증코드 보내기, 이메일 인증, 회원가입 로직
    - MemberMapper
    - MemberMapper.xml
    - MemberService
    - MemberServiceImpl
    - MailService
    - MailConfig: 이메일 API config 설정
    - SecurityConfig: 스프링 시큐리티 설정(추후 수정)
    - Member 관련 request, response
    - InitMock(목업 데이터 생성) 관련 config, impl, mapper
    - GlobalExceptionHandler에 이메일 중복 처리 핸들러 추가
    - build.gradle 의존성 추가 및 정리 #4

--- src/main/java/dorakdorak/global/error/GlobalExceptionHandler.java
+++ src/main/java/dorakdorak/global/error/GlobalExceptionHandler.java
@@ -1,27 +1,39 @@
 package dorakdorak.global.error;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import dorakdorak.global.error.exception.BusinessException;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(BusinessException.class)
-    protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
-        log.error("handleBusinessException", e);
-        final ErrorCode errorCode = e.getErrorCode();
-        final ErrorResponse errorResponse = ErrorResponse.of(errorCode);
-        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
-    }
 
-    @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorResponse> handleException(final Exception e) {
-        log.error("handleException", e);
-        final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+  @ExceptionHandler(BusinessException.class)
+  protected ResponseEntity<ErrorResponse> handleBusinessException(final BusinessException e) {
+    log.error("handleBusinessException", e);
+    final ErrorCode errorCode = e.getErrorCode();
+    final ErrorResponse errorResponse = ErrorResponse.of(errorCode);
+    return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorCode.getStatus()));
+  }
+
+  @ExceptionHandler(Exception.class)
+  protected ResponseEntity<ErrorResponse> handleException(final Exception e) {
+    log.error("handleException", e);
+    final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+    return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(DuplicateKeyException.class)
+  protected ResponseEntity<ErrorResponse> handleDuplicateKeyException(
+      final DuplicateKeyException e) {
+    log.error("handleDuplicateKeyException", e);
+    final ErrorResponse errorResponse = ErrorResponse.of(ErrorCode.DUPLICATED_EMAIL);
+    return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+  }
 }
