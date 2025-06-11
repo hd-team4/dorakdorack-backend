e48edc59bc5b7d8d5d5ca61a597a5ae22fbe40cf
yss0512 <dbtntkd456@naver.com>
2025-06-11 12:07:48 +0900

    [FEAT] JWT 발급 로직 #10
    1. 매 요청마다 JWTFilter에서 Authorization: 액세스 토큰 검증
    2. Authorization 헤더가 없으면 로그인 하지 않은 유저 -> 로그인 진행 로직
    3. LoginFilter: /api/auth/login 요청시 attemptAuthentication 실행
    4. 검증을 위해 CustomMemberDetailsService - loadUserByUsername 실행
        - 검증완료 로그인 성공 LoginFilter - successfulAuthentication
        - 검증실패 로그인 실패 LoginFilter - unsuccessfulAuthentication
    1. JWTUtils: JWT 발급 Util
    2. ReissueController: 액세스 토큰 만료시 액세스 토큰을 재발급 해주는 Controller

---
+++ src/main/java/dorakdorak/domain/auth/api/ReissueController.java
@@ -0,0 +1,109 @@
+package dorakdorak.domain.auth.api;
+
+import dorakdorak.domain.auth.jwt.JWTUtil;
+import dorakdorak.domain.auth.jwt.RedisRefreshToken;
+import dorakdorak.domain.auth.service.RedisRefreshTokenService;
+import dorakdorak.domain.member.service.MemberService;
+import dorakdorak.global.error.ErrorCode;
+import dorakdorak.global.error.exception.BusinessException;
+import io.jsonwebtoken.ExpiredJwtException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class ReissueController {
+
+  private final JWTUtil jwtUtil;
+
+  private final MemberService memberService;
+
+  private final RedisRefreshTokenService redisRefreshTokenService;
+
+  @PostMapping("/reissue")
+  public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+    // get refresh token
+    String refresh = null;
+    Cookie[] cookies = request.getCookies();
+    for (Cookie cookie : cookies) {
+
+      if (cookie.getName().equals("refresh")) {
+        refresh = cookie.getValue();
+      }
+    }
+
+    if (refresh == null) {
+
+      // response status code
+//      return new ResponseEntity<>("refresh token null", HttpStatus.BAD_REQUEST);
+      throw new BusinessException(ErrorCode.INVALID_TOKEN);
+    }
+
+    // expired check
+    try {
+      jwtUtil.isExpired(refresh);
+    } catch (ExpiredJwtException e) {
+
+      // response status code
+//      return new ResponseEntity<>("refresh token expired", HttpStatus.BAD_REQUEST);
+      throw new BusinessException(ErrorCode.INVALID_TOKEN);
+    }
+
+    // 토큰이 refresh인지 확인 (발급시 페이로드에 명시)
+    String category = jwtUtil.getCategory(refresh);
+
+    if (!category.equals("refresh")) {
+
+      // response status code
+//      return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+      throw new BusinessException(ErrorCode.INVALID_TOKEN);
+    }
+
+    // DB에 저장되어 있는지 확인
+    Boolean isExist = redisRefreshTokenService.existsByRefresh(refresh);
+    if (!isExist) {
+      // response body
+//      return new ResponseEntity<>("invalid refresh token", HttpStatus.BAD_REQUEST);
+      throw new BusinessException(ErrorCode.INVALID_TOKEN);
+    }
+
+    String email = jwtUtil.getEmail(refresh);
+    String role = jwtUtil.getRole(refresh);
+    long id = jwtUtil.getId(refresh);
+
+    // make new JWT
+    String newAccess = jwtUtil.createJwt("Authorization", id, email, role, 600000L);
+    String newRefresh = jwtUtil.createJwt("refresh", id, email, role, 86400000L);
+
+    // DB에 기존의 Refresh 토큰 삭제
+    redisRefreshTokenService.deleteByRefresh(refresh);
+
+    // response
+    response.setHeader("Authorization", "Bearer " + newAccess);
+    response.addCookie(createCookie(email, "refresh", newRefresh));
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  private Cookie createCookie(String email, String key, String value) {
+    // 새 Refresh 토큰 저장
+    redisRefreshTokenService.save(new RedisRefreshToken(email, value));
+    // refresh 토큰 DB에 저장
+    memberService.updateMemberRefreshToken(email, value);
+    Cookie cookie = new Cookie(key, value);
+    cookie.setMaxAge(24 * 60 * 60);
+    cookie.setMaxAge(24 * 60 * 60);
+    //cookie.setSecure(true);
+    cookie.setPath("/");
+    cookie.setHttpOnly(true);
+    return cookie;
+  }
+}
---
+++ src/main/java/dorakdorak/domain/auth/service/CustomMemberDetailsService.java
@@ -0,0 +1,36 @@
+package dorakdorak.domain.auth.service;
+
+import dorakdorak.domain.auth.dto.response.CustomMemberDetails;
+import dorakdorak.domain.auth.dto.response.MemberAuthDto;
+import dorakdorak.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomMemberDetailsService implements UserDetailsService {
+
+  private static final Logger log = LoggerFactory.getLogger(CustomMemberDetailsService.class);
+  private final MemberService memberService;
+
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+    //DB에서 조회
+    MemberAuthDto memberAuthDto = memberService.findByEmailIntoAuth(email);
+
+    if (memberAuthDto != null) {
+      //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+      return new CustomMemberDetails(memberAuthDto);
+    }
+
+    return null;
+  }
+
+}
