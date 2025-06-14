c126afe404b2574b3b1e488731285b517b585f52
Wonjun Hong <hwjun12@naver.com>
2025-06-14 20:45:20 +0900

    [FEAT] 대학별 제로웨이스트 랭킹 조회 api 추가

--- src/main/java/dorakdorak/domain/zeroWaste/mapper/ZeroWasteMapper.java
+++ src/main/java/dorakdorak/domain/zeroWaste/mapper/ZeroWasteMapper.java
@@ -1,4 +1,11 @@
 package dorakdorak.domain.zeroWaste.mapper;
 
+import dorakdorak.domain.zeroWaste.dto.response.UniversityRankingResponseDto;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface ZeroWasteMapper {
+
+  List<UniversityRankingResponseDto> findUniversityRankings();
 }
