2ed715ce13fa8a530854cc9d8493ba5487b80622
yeon2lee <md04254@naver.com>
2025-06-17 17:29:31 +0900

    [FEAT] QR 토큰 생성 시 매번 다른 값이 되도록 jti 추가

--- src/main/java/dorakdorak/global/util/jwt/QrTokenProvider.java
+++ src/main/java/dorakdorak/global/util/jwt/QrTokenProvider.java
@@ -2,9 +2,10 @@ package dorakdorak.global.util.jwt;
 
 import io.jsonwebtoken.Claims;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,11 +17,11 @@ public class QrTokenProvider {
   private static final Duration QR_TOKEN_EXPIRE_TIME = Duration.ofDays(3);
 
   public String generateQrToken(Long orderId, Long orderItemId, Long memberId) {
-    Map<String, Object> claims = Map.of(
-        "orderId", orderId,
-        "orderItemId", orderItemId,
-        "memberId", memberId
-    );
+    Map<String, Object> claims = new HashMap<>();
+    claims.put("orderId", orderId);
+    claims.put("orderItemId", orderItemId);
+    claims.put("memberId", memberId);
+    claims.put("jti", UUID.randomUUID().toString());
 
     return jwtUtil.generateToken(claims, "qr-token", QR_TOKEN_EXPIRE_TIME);
   }
